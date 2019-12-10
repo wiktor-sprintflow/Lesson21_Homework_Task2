7e0fa9774e05a040c98dea25da897088c32402ba
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-12-10 16:11:36 +0100

    Lesson21_Homework_task2

---
+++ src/CalculateTextServlet.java
@@ -0,0 +1,46 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/calculateText")
+public class CalculateTextServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String text = request.getParameter("text");
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<i>" + text + "</i>");
+
+        writer.println("<h3> Ilość słów: " + countWords(text) +"</h3>");
+        writer.println("<h3> Ilość znaków: " + text.length() +"</h3>");
+        writer.println("<h3> Ilość znaków (bez spacji): " + countNonWhiteCharacters(text)+"</h3>");
+    }
+    private int countWords(String text){
+        String[] wordsTable = text.split(" ");
+        return wordsTable.length;
+    }
+    private int countNonWhiteCharacters(String text){
+        String[] wordsTable = text.split(" ");
+        int counter = 0;
+        for (String word : wordsTable) {
+            counter+=word.length();
+        }
+        return counter;
+    }
+
+//    private boolean isPalindrom(String text){
+//        String[] wordsTable = text.split(" ");
+//        String[] wordsTableInverted = new String[wordsTable.length];
+//
+//        return false;
+//    }
+}
