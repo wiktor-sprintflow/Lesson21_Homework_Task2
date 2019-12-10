e93606690ec6fcfae39a9ab88311b50b1108a577
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-12-10 16:58:21 +0100

    Adding Palindrom method.

--- src/CalculateTextServlet.java
+++ src/CalculateTextServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @WebServlet("/calculateText")
@@ -20,27 +22,39 @@ public class CalculateTextServlet extends HttpServlet {
         PrintWriter writer = response.getWriter();
         writer.println("<i>" + text + "</i>");
 
-        writer.println("<h3> Ilość słów: " + countWords(text) +"</h3>");
-        writer.println("<h3> Ilość znaków: " + text.length() +"</h3>");
-        writer.println("<h3> Ilość znaków (bez spacji): " + countNonWhiteCharacters(text)+"</h3>");
+        writer.println("<h3> Ilość słów: " + countWords(text) + "</h3>");
+        writer.println("<h3> Ilość znaków: " + text.length() + "</h3>");
+        writer.println("<h3> Ilość znaków (bez spacji): " + countNonWhiteCharacters(text) + "</h3>");
+        writer.println("<h3> Palindrom: " + isPalindrom(text) + "</h3>");
     }
-    private int countWords(String text){
+
+    private int countWords(String text) {
         String[] wordsTable = text.split(" ");
         return wordsTable.length;
     }
-    private int countNonWhiteCharacters(String text){
+
+    private int countNonWhiteCharacters(String text) {
         String[] wordsTable = text.split(" ");
         int counter = 0;
         for (String word : wordsTable) {
-            counter+=word.length();
+            counter += word.length();
         }
         return counter;
     }
 
-//    private boolean isPalindrom(String text){
-//        String[] wordsTable = text.split(" ");
-//        String[] wordsTableInverted = new String[wordsTable.length];
-//
-//        return false;
-//    }
+    private boolean isPalindrom(String text) {
+        String[] wordsTable = text.split(" ");
+        String[] wordsTableClone = wordsTable.clone();
+
+        List<String> stringList = Arrays.asList(wordsTable);
+        List<String> stringList2 = Arrays.asList(wordsTableClone);
+        Collections.reverse(stringList2);
+
+        for (int i = 0; i < stringList.size(); i++) {
+            if (!stringList.get(i).equals(stringList2.get(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
